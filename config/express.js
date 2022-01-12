8a75876e514d190bc5c6a967b9efcf2bea4b998d
Amit Gautam <amitgautam369@gmail.com>
2022-01-12 16:27:14 +0530

    env changes

--- config/express.js
+++ config/express.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: 'ENV_FILENAME' });
 const express = require('express');
 const routes = require('./routes/index-routes');
 const chalk = require('chalk');
